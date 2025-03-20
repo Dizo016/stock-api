3c9f71f6c7e511e7a802209cc41439be9fc71d2b
Diego Silva <diegossilva2008@gmail.com>
2025-03-19 23:38:20 -0300

    Primeiro commit

---
+++ src/main/java/com/adn/StockInfo/StockInfoApplication.java
@@ -0,0 +1,13 @@
+package com.adn.StockInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInfoApplication.class, args);
+	}
+
+}
